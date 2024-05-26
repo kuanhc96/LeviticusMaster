c881867b18a6f017e269bb6a6fb6416406352dbe
kuanhc96 <kuanhc96@uchicago.edu>
2024-05-27 00:36:04 +0800

    repository works with AbstractEntity

--- src/main/java/leviticus/master/repository/IModelParamsRepository.java
+++ src/main/java/leviticus/master/repository/IModelParamsRepository.java
@@ -1,6 +1,8 @@
 package leviticus.master.repository;
 
+import leviticus.master.entity.AbstractEntity;
 import leviticus.master.entity.AbstractModelParamsEntity;
+import org.springframework.data.repository.CrudRepository;
 
-public interface IModelParamsRepository<T extends AbstractModelParamsEntity> extends IBaseRepository<T, Long> {
+public interface IModelParamsRepository extends IBaseRepository<AbstractModelParamsEntity, Long> {
 }
--- src/main/java/leviticus/master/repository/ITrainTaskRepository.java
+++ src/main/java/leviticus/master/repository/ITrainTaskRepository.java
@@ -1,6 +1,7 @@
 package leviticus.master.repository;
 
 import leviticus.master.entity.taskEntity.TrainTaskEntity;
+import org.springframework.data.repository.CrudRepository;
 
 public interface ITrainTaskRepository extends IBaseRepository<TrainTaskEntity, Long> {
 }
