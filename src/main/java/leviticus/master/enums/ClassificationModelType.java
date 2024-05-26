68ad03b744849c0adcc49be749aeb16a95c91735
kuanhc96 <kuanhc96@uchicago.edu>
2024-05-26 08:55:05 +0800

    add enums

--- src/main/java/leviticus/master/enums/ClassificationModelType.java
+++ src/main/java/leviticus/master/enums/ClassificationModelType.java
@@ -1,4 +1,5 @@
 package leviticus.master.enums;
 
 public enum ClassificationModelType {
+    LBP, MINI_VGG
 }
--- src/main/java/leviticus/master/enums/OptimizerType.java
+++ src/main/java/leviticus/master/enums/OptimizerType.java
@@ -1,4 +1,5 @@
 package leviticus.master.enums;
 
 public enum OptimizerType {
+    ADAM, SGD
 }
