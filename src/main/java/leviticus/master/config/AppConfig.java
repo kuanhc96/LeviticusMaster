f8d21f4a8adc987f60899839cd7cd05f2515cd87
kuanhc96 <kuanhc96@uchicago.edu>
2024-07-20 22:35:19 +0800

    config router as a Bean

--- src/main/java/leviticus/master/config/AppConfig.java
+++ src/main/java/leviticus/master/config/AppConfig.java
@@ -1,14 +1,26 @@
 package leviticus.master.config;
 
+import leviticus.master.enums.ClassificationModelType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class AppConfig {
     @Bean
     public WebClient.Builder getWebClientBuilder() {
         return WebClient.builder();
+    }
+
+    @Bean
+    public Map<ClassificationModelType, Integer> getRouter() {
+        Map<ClassificationModelType, Integer> router = new HashMap<>();
+        router.put(ClassificationModelType.LBP, 8000);
+        router.put(ClassificationModelType.MINI_VGG, 8001);
+        return router;
     }
 
 }
