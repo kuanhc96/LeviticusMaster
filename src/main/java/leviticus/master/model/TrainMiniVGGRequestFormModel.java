8b6efa98a08a38efc9b229136620170af8be99de
kuanhc96 <kuanhc96@uchicago.edu>
2024-07-24 21:40:06 +0800

    split train request model into alogirhtm based requests

--- src/main/java/leviticus/master/model/TrainMiniVGGRequestFormModel.java
+++ src/main/java/leviticus/master/model/TrainMiniVGGRequestFormModel.java
@@ -1,4 +1,20 @@
 package leviticus.master.model;
 
-public class TrainMiniVGGRequestFormModel {
+import leviticus.master.enums.ClassificationModelType;
+import leviticus.master.enums.OptimizerType;
+
+public class TrainMiniVGGRequestFormModel extends TrainRequestFormModel {
+    public TrainMiniVGGRequestFormModel() {
+        super();
+        setModelType(ClassificationModelType.MINIVGG);
+    }
+
+    public TrainMiniVGGRequestFormModel(
+            OptimizerType optimizerType,
+            String dataset,
+            Boolean isTrainOnly,
+            Boolean isCrossValidated
+    ) {
+        super(ClassificationModelType.MINIVGG, optimizerType, dataset, isTrainOnly, isCrossValidated);
+    }
 }
