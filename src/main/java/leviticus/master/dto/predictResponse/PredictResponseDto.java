9d1fd26239401668f369f3d2e94c0cfad778efc1
kuanhc96 <kuanhc96@uchicago.edu>
2024-07-31 16:56:31 +0800

    implement PredictResponseDto

--- src/main/java/leviticus/master/dto/predictResponse/PredictResponseDto.java
+++ src/main/java/leviticus/master/dto/predictResponse/PredictResponseDto.java
@@ -1,4 +1,39 @@
 package leviticus.master.dto.predictResponse;
 
+import java.util.Map;
+
 public class PredictResponseDto {
+    private Double accuracy;
+    private String classificationReport;
+    private Map<String, String> predictions;
+
+    public PredictResponseDto(Double accuracy, String classificationReport, Map<String, String> predictions) {
+        this.accuracy = accuracy;
+        this.classificationReport = classificationReport;
+        this.predictions = predictions;
+    }
+
+    public Double getAccuracy() {
+        return accuracy;
+    }
+
+    public void setAccuracy(Double accuracy) {
+        this.accuracy = accuracy;
+    }
+
+    public String getClassificationReport() {
+        return classificationReport;
+    }
+
+    public void setClassificationReport(String classificationReport) {
+        this.classificationReport = classificationReport;
+    }
+
+    public Map<String, String> getPredictions() {
+        return predictions;
+    }
+
+    public void setPredictions(Map<String, String> predictions) {
+        this.predictions = predictions;
+    }
 }
