48a209b5fb0fdf4c1d961595dc95ec3e20870c35
kuanhc96 <kuanhc96@uchicago.edu>
2024-07-31 15:22:10 +0800

    implement PredictRequestDto for other Predict dtos to inherit from

--- src/main/java/leviticus/master/dto/predictRequest/PredictRequestDto.java
+++ src/main/java/leviticus/master/dto/predictRequest/PredictRequestDto.java
@@ -1,4 +1,45 @@
 package leviticus.master.dto.predictRequest;
 
 public class PredictRequestDto {
+
+    private Long trainId;
+    private String trainDataset;
+    private String predictDataset;
+
+    public PredictRequestDto() {}
+
+    public PredictRequestDto(Long trainId) {
+        this.trainId = trainId;
+    }
+
+    public PredictRequestDto(Long trainId, String trainDataset, String predictDataset) {
+        this.trainId = trainId;
+        this.trainDataset = trainDataset;
+        this.predictDataset = predictDataset;
+    }
+
+
+    public Long getTrainId() {
+        return trainId;
+    }
+
+    public void setTrainId(Long trainId) {
+        this.trainId = trainId;
+    }
+
+    public String getTrainDataset() {
+        return trainDataset;
+    }
+
+    public void setTrainDataset(String trainDataset) {
+        this.trainDataset = trainDataset;
+    }
+
+    public String getPredictDataset() {
+        return predictDataset;
+    }
+
+    public void setPredictDataset(String predictDataset) {
+        this.predictDataset = predictDataset;
+    }
 }
