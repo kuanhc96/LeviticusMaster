1956302f963592af73be2d1d042a2f304d9653cb
kuanhc96 <kuanhc96@uchicago.edu>
2024-07-31 16:48:27 +0800

    add findByTrainId for repositories

--- src/main/java/leviticus/master/repository/modelParamsRepository/ICNNModelParamsRepository.java
+++ src/main/java/leviticus/master/repository/modelParamsRepository/ICNNModelParamsRepository.java
@@ -2,5 +2,8 @@ package leviticus.master.repository.modelParamsRepository;
 
 import leviticus.master.entity.modelParamsEntity.CNNModelParamsEntity;
 
+import java.util.Optional;
+
 public interface ICNNModelParamsRepository extends IModelParamsRepository<CNNModelParamsEntity, Long> {
+    Optional<CNNModelParamsEntity> findByTrainId(Long trainId);
 }
--- src/main/java/leviticus/master/repository/modelParamsRepository/ILBPModelParamsRepository.java
+++ src/main/java/leviticus/master/repository/modelParamsRepository/ILBPModelParamsRepository.java
@@ -2,5 +2,8 @@ package leviticus.master.repository.modelParamsRepository;
 
 import leviticus.master.entity.modelParamsEntity.LBPModelParamsEntity;
 
+import java.util.Optional;
+
 public interface ILBPModelParamsRepository extends IModelParamsRepository<LBPModelParamsEntity, Long> {
+    Optional<LBPModelParamsEntity> findByTrainId(Long trainId);
 }
