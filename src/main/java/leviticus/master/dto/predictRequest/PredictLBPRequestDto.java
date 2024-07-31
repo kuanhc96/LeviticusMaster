c6fcc78958a9daee18165bf3c8b6b18a8fb5242e
kuanhc96 <kuanhc96@uchicago.edu>
2024-07-31 15:30:55 +0800

    implement predict lbp request dto

--- src/main/java/leviticus/master/dto/predictRequest/PredictLBPRequestDto.java
+++ src/main/java/leviticus/master/dto/predictRequest/PredictLBPRequestDto.java
@@ -1,4 +1,48 @@
 package leviticus.master.dto.predictRequest;
 
-public class PredictLBPRequestDto {
+public class PredictLBPRequestDto extends PredictRequestDto {
+    private Integer numPoints;
+    private Integer radius;
+
+    public PredictLBPRequestDto() {
+        super();
+    }
+
+    public PredictLBPRequestDto(Long trainId) {
+        super(trainId);
+    }
+
+    public PredictLBPRequestDto(Long trainId, String trainDataset, String predictDataset) {
+        super(trainId, trainDataset, predictDataset);
+
+    }
+
+    public PredictLBPRequestDto(
+            Long trainId,
+            String trainDataset,
+            String predictDataset,
+            Integer numPoints,
+            Integer radius
+    ) {
+        super(trainId, trainDataset, predictDataset);
+        this.numPoints = numPoints;
+        this.radius = radius;
+    }
+
+
+    public Integer getNumPoints() {
+        return numPoints;
+    }
+
+    public void setNumPoints(Integer numPoints) {
+        this.numPoints = numPoints;
+    }
+
+    public Integer getRadius() {
+        return radius;
+    }
+
+    public void setRadius(Integer radius) {
+        this.radius = radius;
+    }
 }
